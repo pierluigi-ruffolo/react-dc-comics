0bef14a63ba95a02e5e72ab75b368ffa7194c0de
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-25 18:30:29 +0100

    feat: migliorie

--- src/componenets/Main.jsx
+++ src/componenets/Main.jsx
@@ -1,9 +1,38 @@
+import ImgByComics from "../assets/img/buy-comics-digital-comics.png";
+import ImgMerchandise from "../assets/img/buy-comics-merchandise.png";
+import ImgShop from "../assets/img/buy-comics-shop-locator.png";
+import ImgSudscriptions from "../assets/img/buy-comics-subscriptions.png";
+import ImgPower from "../assets/img/buy-dc-power-visa.svg";
 export default function Main() {
   return (
-    <main className="bg-black pt-50 pb-50">
-      <div>
+    <main>
+      <div className="bg-black pt-50 pb-50">
         <div className="container">
           <h2 className="color-white">Content goes here</h2>
+        </div>
+      </div>
+      <div class="bg-blue pt-50 pb-50">
+        <div className="container d-flex">
+          <div className="col-main d-flex align-items-center">
+            <img src={ImgByComics} alt="" className="mr-10" />
+            <p className="color-white">DIGITAL COMICS</p>
+          </div>
+          <div className="col-main d-flex align-items-center">
+            <img src={ImgMerchandise} alt="" className="mr-10" />
+            <p className="color-white">DIGITAL COMICS</p>
+          </div>
+          <div className="col-main d-flex align-items-center">
+            <img src={ImgSudscriptions} alt="" className="mr-10" />
+            <p className="color-white">DIGITAL COMICS</p>
+          </div>
+          <div className="col-main d-flex align-items-center">
+            <img src={ImgShop} alt="" className="mr-10" />
+            <p className="color-white">DIGITAL COMICS</p>
+          </div>
+          <div className="col-main d-flex align-items-center">
+            <img src={ImgPower} alt="" className="mr-10" />
+            <p className="color-white">DIGITAL COMICS</p>
+          </div>
         </div>
       </div>
     </main>
