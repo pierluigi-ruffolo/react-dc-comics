bfb5b814997e8d0b75207273e0ceb596dce6292c
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-26 16:27:20 +0100

    feat: aggiungo state header nav

--- src/componenets/NavHeader.jsx
+++ src/componenets/NavHeader.jsx
@@ -1,22 +1,22 @@
 const menuItemsWithLinks = [
-  { id: 1, name: "CHARACTERS", link: "/characters" },
-  { id: 2, name: "COMICS", link: "/comics" },
-  { id: 3, name: "MOVIES", link: "/movies" },
-  { id: 4, name: "TV", link: "/tv" },
-  { id: 5, name: "GAMES", link: "/games" },
-  { id: 6, name: "COLLECTIBLES", link: "/collectibles" },
-  { id: 7, name: "VIDEOS", link: "/videos" },
-  { id: 8, name: "FANS", link: "/fans" },
-  { id: 9, name: "NEW", link: "/new" },
-  { id: 10, name: "SHOP", link: "/shop" },
+  { id: 1, name: "CHARACTERS", link: "/characters", state: true },
+  { id: 2, name: "COMICS", link: "/comics", state: false },
+  { id: 3, name: "MOVIES", link: "/movies", state: true },
+  { id: 4, name: "TV", link: "/tv", state: false },
+  { id: 5, name: "GAMES", link: "/games", state: true },
+  { id: 6, name: "COLLECTIBLES", link: "/collectibles", state: false },
+  { id: 7, name: "VIDEOS", link: "/videos", state: true },
+  { id: 8, name: "FANS", link: "/fans", state: true },
+  { id: 9, name: "NEW", link: "/new", state: false },
+  { id: 10, name: "SHOP", link: "/shop", state: true },
 ];
 
-export default function () {
+export default function NavHeader() {
   return (
     <nav>
       <ul className="d-flex gap-15 wrap">
         {menuItemsWithLinks.map((obj) => (
-          <li key={obj.id}>
+          <li className={obj.state ? "active" : ""} key={obj.id}>
             <a href={obj.link}>{obj.name}</a>
           </li>
         ))}
