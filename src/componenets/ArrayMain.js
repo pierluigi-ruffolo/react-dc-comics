b4e0f6cdd4e140d59dc3c3edae65d3b75acacb2d
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-26 16:08:15 +0100

    feat: inserisco dinamicamente immagini e titolo barra blu main

---
+++ src/componenets/ArrayMain.js
@@ -0,0 +1,35 @@
+import ImgByComics from "../assets/img/buy-comics-digital-comics.png";
+import ImgMerchandise from "../assets/img/buy-comics-merchandise.png";
+import ImgShop from "../assets/img/buy-comics-shop-locator.png";
+import ImgSudscriptions from "../assets/img/buy-comics-subscriptions.png";
+import ImgPower from "../assets/img/buy-dc-power-visa.svg";
+
+const arrey = [
+  {
+    id: 1,
+    name: "digital comics",
+    img: ImgByComics,
+  },
+  {
+    id: 2,
+    name: "Dc merchandise",
+    img: ImgMerchandise,
+  },
+  {
+    id: 3,
+    name: "sybscription",
+    img: ImgShop,
+  },
+  {
+    id: 4,
+    name: "comic shop locator",
+    img: ImgPower,
+  },
+  {
+    id: 5,
+    name: "dc power visa",
+    img: ImgByComics,
+  },
+];
+
+export { arrey };
--- src/componenets/Main.jsx
+++ src/componenets/Main.jsx
@@ -1,12 +1,9 @@
-import ImgByComics from "../assets/img/buy-comics-digital-comics.png";
-import ImgMerchandise from "../assets/img/buy-comics-merchandise.png";
-import ImgShop from "../assets/img/buy-comics-shop-locator.png";
-import ImgSudscriptions from "../assets/img/buy-comics-subscriptions.png";
-import ImgPower from "../assets/img/buy-dc-power-visa.svg";
+import { arrey } from "./ArrayMain";
 import CardMain from "./CardMain";
 
 /* import CardMain from "./CardMain"; */
 export default function Main() {
+  console.log(arrey);
   return (
     <main>
       <div className="hero"></div>
@@ -17,26 +14,15 @@ export default function Main() {
       </div>
       <div className="bg-blue pt-50 pb-50">
         <div className="container d-flex wrap">
-          <div className="col-main-footer d-flex align-items-center">
-            <img src={ImgByComics} alt="" className="mr-10" />
-            <p className="color-white">DIGITAL COMICS</p>
-          </div>
-          <div className="col-main-footer d-flex align-items-center">
-            <img src={ImgMerchandise} alt="" className="mr-10" />
-            <p className="color-white">DIGITAL COMICS</p>
-          </div>
-          <div className="col-main-footer d-flex align-items-center">
-            <img src={ImgSudscriptions} alt="" className="mr-10" />
-            <p className="color-white">DIGITAL COMICS</p>
-          </div>
-          <div className="col-main-footer d-flex align-items-center">
-            <img src={ImgShop} alt="" className="mr-10" />
-            <p className="color-white">DIGITAL COMICS</p>
-          </div>
-          <div className="col-main-footer d-flex align-items-center">
-            <img src={ImgPower} alt="" className="mr-10" />
-            <p className="color-white">DIGITAL COMICS</p>
-          </div>
+          {arrey.map((element) => (
+            <div
+              className="col-main-footer d-flex align-items-center"
+              key={element.id}
+            >
+              <img src={element.img} alt="" className="mr-10" />
+              <p className="color-white">{element.name}</p>
+            </div>
+          ))}
         </div>
       </div>
     </main>
