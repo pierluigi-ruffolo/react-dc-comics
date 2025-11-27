255c6a1e023f56751f4b26c9bd44e1afb2575767
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-27 17:20:27 +0100

    feat: modifico nome cartella

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "./componenets/Header";
-import Main from "./componenets/Main";
-import Footer from "./componenets/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
